eca0e9cd1dc390230e8194a22073f98e6b38d7ef
WildCLown <32888444+WildCLown@users.noreply.github.com>
2019-04-22 10:50:28 -0300

    Adicionadas pessoas

--- PovoamentoCinemaBR.sql
+++ PovoamentoCinemaBR.sql
@@ -1,16 +1,16 @@
 --Inserindo Pessoa;
-INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0000, 'PNome', '00/00/0000', 'dd/mm/yy')
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0001, 'PNome', '01/01/0001', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0002, 'Laion', '29/03/1519', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5103, 'Adiano', '24/03/1998', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5276, 'Leao', '19/03/1998', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4689, 'Jose', '20/06/1996', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5319, 'Gabriel', '22/06/1998', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4371, 'Xuliano', '07/09/1994', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5036, 'Mendonco', '06/03/1997', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (3206, 'Vaval', '30/07/1978', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4482, 'Thiago', '05/12/1994', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0000, 'Jesus', '25/12/0000', 'dd/mm/yy')
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (8932, 'Peter', '02/03/2019', 'dd/mm/yy')
 --Inserindo Telefone_Pessoa;
 
 
