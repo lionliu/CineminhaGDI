593a61f444f9c8dc723400ece5c78959ff579932
WildCLown <32888444+WildCLown@users.noreply.github.com>
2019-04-22 11:23:45 -0300

    Mod Pessoa, Add Endereco
    Modificado pessoas, Adicionado Endereço

--- PovoamentoCinemaBR.sql
+++ PovoamentoCinemaBR.sql
@@ -1,6 +1,6 @@
 --Inserindo Pessoa;
+
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0001, 'PNome', '01/01/0001', 'dd/mm/yy')
-INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0002, 'Laion', '29/03/1519', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5103, 'Adiano', '24/03/1998', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5276, 'Leao', '19/03/1998', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4689, 'Jose', '20/06/1996', 'dd/mm/yy')
@@ -8,13 +8,14 @@ INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5319, 'Gabriel', '22/06/1
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4371, 'Xuliano', '07/09/1994', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (5036, 'Mendonco', '06/03/1997', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (3206, 'Vaval', '30/07/1978', 'dd/mm/yy')
-INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (4482, 'Thiago', '05/12/1994', 'dd/mm/yy')
 INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0000, 'Jesus', '25/12/0000', 'dd/mm/yy')
-INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (8932, 'Peter', '02/03/2019', 'dd/mm/yy')
+
 --Inserindo Telefone_Pessoa;
+
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (5276,999258866)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (5276,999256688)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (5276,988669925)
+
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (5319,998074926)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (4689,979109563)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (5103,997693261)
@@ -23,16 +24,18 @@ INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (0000,940028922)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (8932,999890929)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (0001,000000001)
 INSERT INTO Telefone_Pessoa (CPF_Telefone,telefone) VALUES (3206,932429666)
+
 --Inserindo Endereco_Pessoa;
 
-
-
-
-
-
-
-
-
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (0001,0001,'Teste','De','Endereco',001)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (5276,4232,'Lincoln Road','Miami Beach','Miami',786)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (4371,4216,'Das dores','Casa Forte','Recife',786)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (4689,4891,'Teste','De','Endereco',001)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (5319,7843,'Tejucupapo','San Martin','Recife',533)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (4371,5123,'SQN102','Norte','Brasilia',133)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (5036,3321,'Das gracas','Varzea','Recife',421)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (3206,1521,'Setubal','Boa Viagem','Recife',6616)
+INSERT INTO Endereco_Pessoa (CPF_Endereco,CEP,Rua,Bairro,Cidade,Numero_Endereco) VALUES (0000,0000,'Fazendinha','De','Jerusalem',404)
 
 --Inserindo Funcionario;
 
