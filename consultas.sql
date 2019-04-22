f9f334a093fc47e37cc45732858b2fff63b1a80d
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2019-04-22 11:36:09 -0300

    Selecao com uso do LIKE
    Queremos buscar na tabela de enderecos, enderecos que tenham como cidade, cidades que comecam com Rec.

--- consultas.sql
+++ consultas.sql
@@ -4,11 +4,18 @@ SELECT AVG (I.Preco_Ingresso) FROM Ingresso I;
 
 SELECT MIN (I.Preco_Ingresso) FROM Ingresso I;
 
-SELECT (*) FROM Ingresso I WHERE I.Preco_Ingresso < COLOCARVALOR; 
+SELECT (*) 
+FROM Ingresso I 
+WHERE I.Preco_Ingresso < COLOCARVALOR; 
 
 SELECT COUNT I.ID_Filme, I.Horario 
 FROM Ingresso I 
 WHERE I.Preco_Ingresso > COLOCARVALOR 
 AND I.Horario IN ('METRICA1', 'METRICA2')
 AND I.ID_Filme BETWEEN METRICA1 AND METRICA2;
+
+SELECT EP.Bairro, EP.Cidade 
+FROM Endereco_Pessoa EP 
+WHERE EP.Cidade 
+LIKE "Rec%";
 
