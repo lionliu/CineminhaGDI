d49b91c86c21f147363d5af931958ed22e81ad66
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2019-04-22 10:41:19 -0300

    Seleção com SELECT-FROM-WHERE, COUNT, IN e BETWEEN
    Testar com povoamento.

--- consultas.sql
+++ consultas.sql
@@ -1,5 +1,11 @@
-SELECT MAX (Numero_Salas) FROM Cinema;
+SELECT MAX (C.Numero_Salas) FROM Cinema C;
 
-SELECT AVG (Preco_Ingresso) FROM Ingresso;
+SELECT AVG (I.Preco_Ingresso) FROM Ingresso I;
 
-SELECT MIN (Preco_Ingresso) FROM Ingresso;
+SELECT MIN (I.Preco_Ingresso) FROM Ingresso I;
+
+SELECT COUNT I.ID_Filme, I.Horario 
+FROM Ingresso I 
+WHERE I.Preco_Ingresso > COLOCARVALOR 
+AND I.Horario IN ('METRICA1', 'METRICA2')
+AND I.ID_Filme BETWEEN METRICA1 AND METRICA2;
