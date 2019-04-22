0d0ce05e605c1ef001ad6c720fd0dec081b0ea0b
WildCLown <32888444+WildCLown@users.noreply.github.com>
2019-04-22 10:34:57 -0300

    Povoamento
    Adicionado arquivo para começar povoamento

---
+++ PovoamentoCinemaBR.sql
@@ -0,0 +1,228 @@
+--Inserindo Pessoa;
+INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nasc) VALUES (0000, 'PNome', '00/00/0000', 'dd/mm/yy')
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Telefone_Pessoa;
+
+
+
+
+
+
+
+
+
+
+--Inserindo Endereco_Pessoa;
+
+
+
+
+
+
+
+
+
+
+--Inserindo Funcionario;
+
+
+
+
+
+
+
+
+--Inserindo Setor;
+
+
+
+
+
+
+
+
+
+
+--Inserindo Funcionario_Supervisor;
+
+
+
+
+
+
+
+
+--Inserindo Pessoa_Funcionario;
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Cinema;
+
+
+
+
+
+
+
+
+
+--Inserindo Endereco_Cinema;
+
+
+
+
+
+
+
+--Inserindo Telefone_Cinema;
+
+
+
+
+
+
+
+
+--Inserindo Trabalha;
+
+
+
+
+
+
+
+
+--Inserindo Snack_Bar;
+
+
+
+
+
+
+
+
+
+--Inserindo Produto;
+
+
+
+
+
+
+
+
+
+--Inserindo Sala;
+
+
+
+
+
+
+
+
+--Inserindo Filme;
+
+
+
+
+
+
+
+
+
+--Inserindo Filme_Sala;
+
+
+
+
+
+
+
+
+
+--Inserindo Genero;
+
+
+
+
+
+
+
+
+--Inserindo Diretor;
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Ator;
+
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Ingresso;
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Vende_Ingresso;
+
+
+
+
+
+
+
+
+
+
+
+--Inserindo Exibe;
+
+
+
+
+
+
+
+
