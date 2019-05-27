0485309e2e89fd4ea59dbd5e7a353ee802489d37
Gabriel Teixeira Soares de Almeida <gtsa@WINDOWS.CIN.UFPE.BR>
2019-05-27 17:20:01 -0300

    Realizado povoamento de VARRAY, modificada tabela de pessoa, atendendo VARRAY.

--- OutroReitor/PovoamentoOR.sql
+++ OutroReitor/PovoamentoOR.sql
@@ -1,1 +1,8 @@
 --Povoamento de OR
+INSERT INTO TB_PESSOA VALUES (
+    TP_PESSOA(
+        1001,'Leao',to_date('19/03/1998', 'dd/mm/yy'),TP_ENDERECO(2001,'Lincoln Road','Miami Beach','Miami',005),TP_FONES(
+            TP_FONE(999258866),TP_FONE(999256688),TP_FONE(988669925)
+            )
+    )
+);
