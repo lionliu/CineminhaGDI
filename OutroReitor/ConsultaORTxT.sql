1083f5c087c4e7a9318ff11ca9dcfaf616032773
WildClown <gtsa@cin.ufpe.br>
2019-05-28 20:39:33 -0300

    Realizado povoamento NT, e consulta NT.

--- OutroReitor/ConsultaORTxT.sql
+++ OutroReitor/ConsultaORTxT.sql
@@ -1,3 +1,18 @@
 --Coleta CPF, NOME, e os seus números de telefone (isso em ARRAY)
 SELECT P.CPF,P.NOME_PESSOA, T.*
 FROM TB_PESSOA P, TABLE(P.L_FONES) T;
+
+SELECT F.ID_Filme, F.Faixa_Etaria, F.Data_Inicio, F.Data_Fim, F.Diretor, F.Genero, F.Nome_Filme, T.*
+FROM TB_FILME F, TABLE(F.Atores) T;
+
+CREATE OR REPLACE TYPE TP_FILM AS OBJECT( 
+    ID_Filme INTEGER, 
+    Faixa_Etaria NUMBER, 
+    Data_Inicio DATE, 
+    Data_Fim DATE, 
+    Diretor VARCHAR(30), 
+    Genero VARCHAR(30), 
+    Nome_Filme VARCHAR(30), 
+    Atores TP_NT_ATOR 
+); 
+/
--- OutroReitor/PovoamentoOR.sql
+++ OutroReitor/PovoamentoOR.sql
@@ -1,8 +1,23 @@
 --Povoamento de OR
 INSERT INTO TB_PESSOA VALUES (
     TP_PESSOA(
-        1001,'Leao',to_date('19/03/1998', 'dd/mm/yy'),TP_ENDERECO(2001,'Lincoln Road','Miami Beach','Miami',005),TP_FONES(
+        1001,'Leao',to_date('19/03/1998', 'dd/mm/yyyy'),TP_ENDERECO(2001,'Lincoln Road','Miami Beach','Miami',005),TP_FONES(
             TP_FONE(999258866),TP_FONE(999256688),TP_FONE(988669925)
+            )
+    )
+);
+INSERT INTO TB_FILME VALUES(
+    TP_FILM(
+        3001,
+        16,
+        to_date('28/05/2019','dd/mm/yyyy'),
+        to_date('28/06/2019','dd/mm/yyyy'),
+        'Chad Stahelski','Acao','John Wick 4',
+        TP_NT_ATOR(
+            TP_ATOR('Keanu Reeves'),
+            TP_ATOR('Halle Berry'),
+            TP_ATOR('Anjelica Huston'),
+            TP_ATOR('Adiano')
             )
     )
 );
