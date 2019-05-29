fbbaaa7ad49b166818f58dd5a4f14f39122461c8
Leao Liu Masur <llm2@cin.ufpe.br>
2019-05-29 20:54:24 -0300

    Consulta com VALUE() e comparaPreco() n pegando.

--- OutroReitor/ConsultaORTxT.sql
+++ OutroReitor/ConsultaORTxT.sql
@@ -6,3 +6,25 @@ SELECT F.ID_Filme, F.Faixa_Etaria, F.Data_Inicio, F.Data_Fim, F.Diretor, F.Gener
 FROM TB_FILME F, TABLE(F.Atores) T;
 
 SELECT DEREF(SB.CINEMA) AS CINEMA FROM TB_SNACK_BAR SB;
+
+DECLARE
+mp TP_PRODUTO;
+m DECIMAL;
+
+BEGIN
+SELECT VALUE(p) INTO mp FROM TB_PRODUTO p
+WHERE p.ID_Produto = 50; -- Pipoca
+
+SELECT d.comparaPreco(mp) INTO m FROM TB_PRODUTO d WHERE
+d.ID_Produto = 52; -- Texero Acai
+
+IF m > 0 THEN DBMS_OUTPUT.PUT_LINE('Texero Acai' || ' tem preco MAIOR do que '
+||TO_CHAR(mp.Nome_Produto) ); END IF;
+
+IF m = 0 THEN DBMS_OUTPUT.PUT_LINE('Texero Acai' || ' tem preco IGUAL ao '
+||TO_CHAR(mp.Nome_Produto) ); END IF;
+
+IF m < 0 THEN DBMS_OUTPUT.PUT_LINE('Texero Acai' || ' tem preco MENOR do que '
+||TO_CHAR(mp.Nome_Produto) );END IF;
+
+END;
