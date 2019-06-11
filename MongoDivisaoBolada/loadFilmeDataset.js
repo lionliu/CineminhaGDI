82a0aa058f26503205400dcb18dc7bb15f66bd26
AdrianoCabral <afcf@cin.ufpe.br>
2019-06-11 11:26:21 -0300

    Inserido com sucesso pelo terminal, nao aparecer no compass

--- MongoDivisaoBolada/loadFilmeDataset.js
+++ MongoDivisaoBolada/loadFilmeDataset.js
@@ -1,3 +1,3 @@
-db = db.getSiblingDB("cineminha");
+db = db.getSiblingDB("Cineminha");
 db.movies.drop();
-db.movies.insertOne([{"ID_Filme": 1401, "Faixa_Etaria": 14, "Data_Inicio": "24/04/2019", "Data_Fim": "27/06/2019", "Nome_Filme": "Vingadores"}]);
+db.movies.insertOne({"ID_Filme": 1401, "Faixa_Etaria": 14, "Data_Inicio": "24/04/2019", "Data_Fim": "27/06/2019", "Nome_Filme": "Vingadores"});
