6572a5ff88ea740db6e86283fc4a176c6df8006f
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2019-06-11 14:37:07 -0300

    ze povoamento + sort + aggregate AVG + atualizacao + pretty + sort

--- mongoLdb/atualizacao.js
+++ mongoLdb/atualizacao.js
@@ -1,1 +1,2 @@
 //checklist 3: atualizacao de documento
+db.movies.updateOne({"Nome_Filme":"Dragon Ball Zé"},{%set:{"visualizações":"mais de 8000"}});
--- mongoLdb/funcoesagregacao.js
+++ mongoLdb/funcoesagregacao.js
@@ -1,1 +1,5 @@
 //checklist 6: uso de funcao de agregacao: sum, avg, count e max
+//avg
+db.movies.aggregate(
+    {$group:{_id: "Nome_Filme", nota_media:{$avg: "$AdoroCinema.Critica"}}}
+);
