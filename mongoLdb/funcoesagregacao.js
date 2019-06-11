c8212aa2676d6c60f24458b5b2c064a1f36dbd9b
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2019-06-11 15:31:39 -0300

    avg nao funfou como esperado
    boa sorte

--- mongoLdb/funcoesagregacao.js
+++ mongoLdb/funcoesagregacao.js
@@ -1,5 +1,3 @@
 //checklist 6: uso de funcao de agregacao: sum, avg, count e max
 //avg
-db.movies.aggregate(
-    {$group:{_id: "$Nome_Filme", nota_media:{$avg: "$AdoroCinema.Critica"}}}
-);
+db.movies.aggregate(   {$group:{_id: "$AdoroCinema", nota_media:{$avg: "$Critica" }}})
