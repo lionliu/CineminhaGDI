61912b72ee2608809d5730196e1bdece4a7559d8
AdrianoCabral <afcf@cin.ufpe.br>
2019-06-11 17:25:34 -0300

    funcao de agrupamento dos filmes por faixa etaria

--- mongoLdb/aggregation_functions.js
+++ mongoLdb/aggregation_functions.js
@@ -1,1 +1,2 @@
-db.movies.aggregate([{$group:{_id:0, mediaEtaria: {$avg: "$Faixa_Etaria"}}}])
+db.movies.aggregate([{$group:{_id:0, mediaEtaria: {$avg: "$Faixa_Etaria"}}}]);
+db.movies.aggregate([ {$match: {}}, {$group:{_id:"$Nome_Filme", censura:{$avg: "$Faixa_Etaria"}}}])
