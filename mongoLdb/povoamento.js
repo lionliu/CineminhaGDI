9a122f1bf23d2151b32dda0f0a3c2ef52df39ca4
Gabriel Teixeira Soares de Almeida <gtsa@windows.cin.ufpe.br>
2019-06-11 12:05:08 -0300

    Adicionado Insert incompleto de Texero.

--- mongoLdb/povoamento.js
+++ mongoLdb/povoamento.js
@@ -1,6 +1,6 @@
 //checklist 1: criacao de ma colecao de documentos
 //zé aabaixo
-db.movieDetails.insertOne({
+db.movieDetails.insertOne({//Insert Zé
     "ID_Filme" : 1212,
     "Faixa_Etaria" : "livre",
     "Data_Inicio" : "25/07/2003",
@@ -14,6 +14,19 @@ db.movieDetails.insertOne({
     "Data_Fim" : "20/01/2000",
     "Nome_Filme" : "Toy Story 2"
 });
-
+db.movieDetails.insertOne({//Insert Texero
+		"ID_FILME" : 1214,
+		"Faixa_Etaria" : "14",
+		"Data_Inicio" : "21/06/2004",
+		"Data_Fim" : "23/07/2004",
+		"Nome_Filme" : "A história do IPoop."
+});
+db.movieDetails.insertOne({
+		"ID_FILME" : 1215,
+		"Faixa_Etaria" : "16",
+		"Data_Inicio" : "11/08/2006",
+		"Data_Fim" : "23/10/2006",
+		"Nome_Filme" : "Ana, a Bele"
+});
 //vou atualizar adicionando diretor e atores nesses 2
 //zé acima
