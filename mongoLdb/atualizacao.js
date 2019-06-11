6c5e42339a7c17e7bc652e9dcf97e9f1e622472f
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2019-06-11 15:00:10 -0300

    atualizacao funcionando

--- mongoLdb/atualizacao.js
+++ mongoLdb/atualizacao.js
@@ -1,2 +1,2 @@
 //checklist 3: atualizacao de documento
-db.movies.updateOne({"Nome_Filme":"Dragon Ball Zé"},{%set:{"visualizações":"mais de 8000"}});
+db.movies.updateOne({"Nome_Filme":"Dragon Ball Zé"},{$set:{"visualizações":"mais de 8000"}},{"upsert":true})
