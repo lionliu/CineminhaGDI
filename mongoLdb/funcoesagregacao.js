11c24e3e0bdc7096bb4eea428f469f4c5dfb3f80
Leao Liu Masur <llm2@WINDOWS.CIN.UFPE.BR>
2019-06-11 15:42:09 -0300

    Adicionado aggregate com count

--- mongoLdb/funcoesagregacao.js
+++ mongoLdb/funcoesagregacao.js
@@ -1,3 +1,20 @@
 //checklist 6: uso de funcao de agregacao: sum, avg, count e max
 //avg
 db.movies.aggregate(   {$group:{_id: "$AdoroCinema", nota_media:{$avg: "$Critica" }}})
+
+//count
+// Retorna a quantidade de notas do Adoro Cinema maiores que 
+db.movies.aggregate(
+    [
+      {
+        $match: {
+          "AdoroCinema.AdoroCinema": {
+            $gte: 4
+          }
+        }
+      },
+      {
+        $count: "Notas>=4"
+      }
+    ]
+  )
