b24593521faac79b2aceca226e7f8ad2e0889e61
AdrianoCabral <afcf@cin.ufpe.br>
2019-06-11 16:47:37 -0300

    criacao de arquivo das funcoes de agregacao

--- MongoDivisaoBolada/loadFilmeDataset.js
+++ MongoDivisaoBolada/loadFilmeDataset.js
@@ -1,3 +1,24 @@
 db = db.getSiblingDB("Cineminha");
-db.movies.drop();
-db.movies.insertOne({"ID_Filme": 1401, "Faixa_Etaria": 14, "Data_Inicio": "24/04/2019", "Data_Fim": "27/06/2019", "Nome_Filme": "Vingadores"});
+db.movieDetails.drop();
+db.moviesDetails.insertMany([
+	{
+	"Faixa_Etaria": 14,
+	"Data_Inicio": "24/04/2019",
+	"Data_Fim": "27/06/2019",
+	"Nome_Filme": "Vingadores"
+	"Diretor": "Irmaos Russo"
+	},
+	{
+	"Faixa_Etaria": 18,
+	"Data_Inicio": "06/05/2019",
+	"Data_Fim": "22/06/2019",
+	"Nome_Filme": "John Wick 3: Parabellum"
+	"Diretor"
+	},
+	{
+	"Faixa_Etaria": 12,
+	"Data_Inicio": "06/06/2019",
+	"Data_Fim": "09/07/2019",
+	"Nome_Filme": "X-Men: Fenix Negra"
+	}
+]);
